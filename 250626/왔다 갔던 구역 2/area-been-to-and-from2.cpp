5e8f42c9b2dd27b18e3dbd9bbcaacec09cef5c1e
YoungJun <85381075+YoungJJun@users.noreply.github.com>
2025-06-26 15:43:28 +0900

    Title: 왔다 갔던 구역 2, Time: 7ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 250626/왔다 갔던 구역 2/area-been-to-and-from2.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+
+using namespace std;
+
+int n;
+int arr[2000] = {0,};
+char dir;
+int dist;
+
+int x=1000;
+
+int main() {
+    cin >> n;
+
+    for (int i = 0; i < n; i++) {
+        cin >> dist >> dir;
+
+        if(dir == 'L'){
+            while(dist--){
+                x--;
+                arr[x]++;
+                
+            }
+            
+        }
+        else{ //direction == 'R'
+            while(dist--){
+                x++;
+                arr[x]++;
+                
+            }
+        
+        }
+        
+    }
+
+
+    int cnt=0;    
+    for(int i=0; i<2000; i++){
+        if(arr[i]>1){
+            cnt++;
+        }
+    }
+
+    cout<<cnt;
+
+    return 0;
+}
